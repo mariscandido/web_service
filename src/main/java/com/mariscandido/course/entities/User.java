19fc898665444317e8da65072b8520eeb6059b81
Mariana Silveira <mariscandido3107@gmail.com>
2023-10-06 06:13:50 -0300

    User entity and resource

---
+++ src/main/java/com/mariscandido/course/entities/User.java
@@ -0,0 +1,86 @@
+package com.mariscandido.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String phone;
+	private String password;
+
+	public User() {
+	}
+
+	public User(Long id, String nome, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
---
+++ src/main/java/com/mariscandido/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.mariscandido.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mariscandido.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
