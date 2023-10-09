733d7f7dde84136396cfac7f35f770787c947e23
Mariana Silveira <mariscandido3107@gmail.com>
2023-10-09 09:10:58 -0300

    OrderStatus

--- src/main/java/com/mariscandido/course/entities/Order.java
+++ src/main/java/com/mariscandido/course/entities/Order.java
@@ -41,7 +41,7 @@ public class Order implements Serializable {
 		super();
 		this.id = id;
 		this.moment = moment;
-		this.orderStatus = orderStatus;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
