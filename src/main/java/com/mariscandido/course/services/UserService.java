ee4965c0d515b2f8d8b4f36e14a1f4aa37aea8dd
Mariana Silveira <mariscandido3107@gmail.com>
2023-10-09 07:07:43 -0300

    Service layer, component registration

---
+++ src/main/java/com/mariscandido/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.mariscandido.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mariscandido.course.entities.User;
+import com.mariscandido.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> FindAll(){
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
