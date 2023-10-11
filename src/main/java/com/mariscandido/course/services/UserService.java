b3edee2c6ace41de97077906ac3f8436ed2d25a3
Mariana Silveira <mariscandido3107@gmail.com>
2023-10-11 17:49:08 -0300

    Exception handling - update

--- src/main/java/com/mariscandido/course/services/UserService.java
+++ src/main/java/com/mariscandido/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.mariscandido.course.repositories.UserRepository;
 import com.mariscandido.course.services.exceptions.DatabaseException;
 import com.mariscandido.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
